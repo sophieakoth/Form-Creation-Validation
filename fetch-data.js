34c32cabe480a7f0f88ef8ee14e682b56e08aacf
Sophie Akoth <akothsophie9023.gmail.com>
2025-10-01 14:10:23 +0300

    Added fetch-data.js

---
+++ fetch-data.js
@@ -0,0 +1,29 @@
+async function  fetchUserData(params) {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById("api-data");
+
+    try{
+        const response = await fetch(apiUrl);
+        const users = await response. json();
+
+        dataContainer.innerHTML = '.';
+     const userList = document.createElement("ul");
+
+     users.forEach(user => {
+        const listItem = document.createElement("li");
+        listItem.textContent = user.name;
+        userList.appendChild(listItem);
+     });
+     dataContainer.appendChild(userList);
+          
+    }catch{
+        dataContainer.innerHTML = '.';
+        dataContainer.textContent = 'Failed to load user data.';
+        console.error('Error fetching data;' ,error);
+    }
+
+   
+}
+
+
+document.addEventListener( 'DOMContentLoaded',fetchUserData);
